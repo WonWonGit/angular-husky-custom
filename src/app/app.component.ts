8212c7f10513bfc1e424f8f97a90e359980ee764
suwon baek <suwon_baek@Suwons-MacBook-Pro.local>
2023-08-04 10:15:23 +0900

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,17 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'angular-eslint-husky-test';
+
+  test() {
+    console.log('hello');
+    console.log('hello');
+    console.log('hello');
+    console.log('hello');
+    console.log('hello');
+    console.log('hello');
+  }
 }
